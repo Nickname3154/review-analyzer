8b0f8f1b71729162d6d6428ddd810734bef2540f
Nickname3154 <rladudwl72@gmail.com>
2025-06-04 05:54:47 +0000

    test

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,3 +1,21 @@
-sudo apt update && sudo apt install -y libnss3 libxss1 libappindicator1 libindicator7 \
-fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 \
-libgdk-pixbuf2.0-0 libnspr4 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 xdg-utils
+#!/bin/bash
+
+# 필요한 시스템 라이브러리 설치
+sudo apt update && sudo apt install -y \
+  libnss3 \
+  libxss1 \
+  libappindicator1 \
+  libindicator7 \
+  fonts-liberation \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libgdk-pixbuf2.0-0 \
+  libnspr4 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils

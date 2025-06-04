18966563819a31ba70d52f1a5a0ea290c06b5701
Nickname3154 <rladudwl72@gmail.com>
2025-06-04 06:02:32 +0000

    test

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,3 +1,7 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt update
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
 #!/bin/bash
 
 # 필요한 시스템 라이브러리 설치
